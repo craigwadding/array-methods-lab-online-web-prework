0d8d01c0019cfc9ffa8623c024f90aae0bae387e
craig wadding <craig.wadding@gmail.com>
2019-03-24 23:41:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,24 +4,24 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-  array.sort(element)
+  array.sort
 
 end
 
 def using_reverse(array)
-  array.reverse(element)
+  array.reverse
 
 end
 
 def using_first(array)
-  array.first(array)
+  array.first
 
 end
 
 def using_last(array)
-  array.last(array)
+  array.last
 end
 
 def using_size(array)
-  array.size(array)
+  array.size
 end
